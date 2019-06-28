65e06fce7787edf47fd52def1f81c1b4bb6dfea1
Nanda Okitavera <codeharuka.yusa@gmail.com>
2019-06-28 21:39:05 +0700

    envsetup: build dtbo.img after compile the kernel

--- envsetup.sh
+++ envsetup.sh
@@ -8,8 +8,10 @@
 KERNEL_DIR=$(dirname "$(readlink -f "$0")")
 BUILD_DIR=$KERNEL_DIR/out
 
-export DTC_EXT=dtc
 export KBUILD_COMPILER_STRING=$(clang -v |& sed -e '1!d;s/ (http.\+)//g')
+
+export DTC_EXT=dtc # from https://android.googlesource.com/platform/external/dtc/+/refs/heads/pie-release
+export DTPY=mkdtboimg.py # from https://android.googlesource.com/platform/system/libufdt/+/refs/heads/pie-release
 
 export OLDPROMPT="$PS1"
 export PS1="(dev) $OLDPROMPT"
@@ -25,6 +27,21 @@ mke(){
     ARCH=arm64 \
     CROSS_COMPILE=aarch64-linux-gnu- \
     CC=clang CLANG_TRIPLE=aarch64-linux-gnu- $@ |& tee ../mke.log
+  
+  test -z "$@" && mke_dtimg
+}
+
+mke_dtimg(){
+  DTOUT=$BUILD_DIR/dtbo.img
+  MKDT=$(which $DTPY)
+
+  if [[ -n "$MKDT" ]];then
+    echo "Building overlay dt using $MKDT"
+    python2 $MKDT \
+      create $DTOUT \
+      $(find $BUILD_DIR/arch/arm64/boot/dts/ -iname '*-overlay.dtbo' -print)
+    echo "Build done: $DTOUT"
+  fi
 }
 
 cherry-am(){
@@ -36,5 +53,5 @@ cherry-am(){
 deactivate(){
   export PS1="$OLDPROMPT"
   unset DTC_EXT KBUILD_COMPILER_STRING OLDPROMPT
-  unset -f mke cherry-am deactivate
+  unset -f mke mke_dtimg cherry-am deactivate
 }
