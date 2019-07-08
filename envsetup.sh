bab163aa9f2ab880f38ce4757c632aaf704089d7
Pietro Ivan Cerrato <ivan@cerrato.biz>
2019-07-08 18:15:01 +0200

    Minimal adjustments for my machine

--- envsetup.sh
+++ envsetup.sh
@@ -1,57 +1,40 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
 ((return 0 2>/dev/null) || [[ $ZSH_EVAL_CONTEXT =~ :file$ ]]) || {
   echo "Please load the file instead."
   echo "example: source envsetup.sh"
   exit 1
 }
 
-KERNEL_DIR=$(dirname "$(readlink -f "$0")")
+KERNEL_DIR="/Volumes/mi9se/kowalski-grus"
 BUILD_DIR=$KERNEL_DIR/out
 
-export KBUILD_COMPILER_STRING=$(clang -v |& sed -e '1!d;s/ (http.\+)//g')
-
-export DTC_EXT=dtc # from https://android.googlesource.com/platform/external/dtc/+/refs/heads/pie-release
-export DTPY=mkdtboimg.py # from https://android.googlesource.com/platform/system/libufdt/+/refs/heads/pie-release
-
-export OLDPROMPT="$PS1"
-export PS1="(dev) $OLDPROMPT"
+export PATH=/usr/local/opt/coreutils/libexec/gnubin:/Volumes/mi9se/toolchains/gcc/bin:$PATH
+export ARCH=arm64
+export CROSS_COMPILE=aarch64-linux-android-
+export DTC_EXT=/usr/local/bin/dtc
+export DTPY=/usr/local/bin/mkdtboimg.py
 
 mke(){
   mkdir -p $BUILD_DIR
-  version=$(git rev-list --tags --max-count=1 | xargs git describe --tags)
-  echo "-$version" > {$KERNEL_DIR,$BUILD_DIR}/.scmversion
+  make O=$BUILD_DIR grus_user_defconfig
+  make -j4 O=$BUILD_DIR CC=/Volumes/mi9se/toolchains/clang/bin/clang CLANG_TRIPLE=aarch64-linux-gnu-
 
-  make -j$(nproc --all) \
-    -C $KERNEL_DIR \
-    O=$BUILD_DIR \
-    ARCH=arm64 \
-    CROSS_COMPILE=aarch64-linux-gnu- \
-    CC=clang CLANG_TRIPLE=aarch64-linux-gnu- $@ |& tee ../mke.log
-  
-  test -z "$@" && mke_dtimg
+  if [ $? -eq 0 ]
+  then 
+    mke_dtimg
+  fi
 }
 
 mke_dtimg(){
-  DTOUT=$BUILD_DIR/dtbo.img
-  MKDT=$(which $DTPY)
+  DTOUT=$BUILD_DIR/arch/arm64/boot/dtbo.img
 
   if [[ -n "$MKDT" ]];then
     echo "Building overlay dt using $MKDT"
-    python2 $MKDT \
+    python2.7 $DTPY \
       create $DTOUT \
       $(find $BUILD_DIR/arch/arm64/boot/dts/ -iname '*-overlay.dtbo' -print)
     echo "Build done: $DTOUT"
   fi
 }
 
-cherry-am(){
-  co=cherry.patch
-  uri=$(echo $1|cut -d\# -f1)
-  curl -o $co $uri.patch && git am < $co && rm $co
-}
-
-deactivate(){
-  export PS1="$OLDPROMPT"
-  unset DTC_EXT KBUILD_COMPILER_STRING OLDPROMPT
-  unset -f mke mke_dtimg cherry-am deactivate
-}
