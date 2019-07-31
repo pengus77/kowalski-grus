7a398d678b2726778c0beb42f80b422df0fa733d
Pietro Ivan Cerrato <ivan@cerrato.biz>
2019-07-31 14:24:11 +0200

    Disable user space write acces to pm_qos latencies

--- kernel/power/qos.c
+++ kernel/power/qos.c
@@ -874,6 +874,7 @@ static ssize_t pm_qos_power_read(struct file *filp, char __user *buf,
 static ssize_t pm_qos_power_write(struct file *filp, const char __user *buf,
 		size_t count, loff_t *f_pos)
 {
+/*
 	s32 value;
 	struct pm_qos_request *req;
 
@@ -890,7 +891,7 @@ static ssize_t pm_qos_power_write(struct file *filp, const char __user *buf,
 
 	req = filp->private_data;
 	pm_qos_update_request(req, value);
-
+*/
 	return count;
 }
 
