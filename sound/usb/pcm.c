af3d2d0b57a01d43c4dccd2abd1adbde5d842acc
Pietro Ivan Cerrato <ivan@cerrato.biz>
2020-01-16 14:55:51 +0100

    sound: usb: remove xiaomi code

--- sound/usb/pcm.c
+++ sound/usb/pcm.c
@@ -554,10 +554,6 @@ static int set_format(struct snd_usb_substream *subs, struct audioformat *fmt)
 			dev_err(&dev->dev,
 				"%d:%d: usb_set_interface failed (%d)\n",
 				fmt->iface, fmt->altsetting, err);
-			if((0x2717 == USB_ID_VENDOR(subs->stream->chip->usb_id))&&(0x3801 == USB_ID_PRODUCT(subs->stream->chip->usb_id)))
-			{
-				kick_usbpd_vbus_sm();
-			}
 			return -EIO;
 		}
 		dev_dbg(&dev->dev, "setting usb interface %d:%d\n",
